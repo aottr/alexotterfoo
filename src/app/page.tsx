cd61fcff2faa58bcef93fbfaa9faee45fe396dcc
Dustin Kroeger <dustin.kroeger@telekom.de>
2024-01-04 01:34:08 +0100

    👌 IMPROVE: Update next.config.js and package.json, and modify page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import { IconBrandMastodon, IconBrandTwitter, IconBrandDiscord, IconBrandTelegra
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center py-12 px-6 antialiased font-mono">
       <div className="flex flex-col items-center">
         <Image
           src="/avatar.jpg"
           alt="Vercel Logo"
-          className="rounded-full ring ring-4 ring-accent ring-offset-base-100 w-56"
+          className="rounded-full ring-4 ring-accent ring-offset-base-100 w-56"
           width={200}
           height={200}
           priority
@@ -19,48 +19,48 @@ export default function Home() {
         <h1 className="text-4xl font-bold mb-10 mt-6 text-accent">Alex</h1>
       </div>
       <h2 className="text-2xl font-bold mb-4 text-white">Socials</h2>
-      <div className="grid md:grid-cols-2 gap-4">
-        <a href="link1" className='btn btn-lg btn-outline btn-accent btn-block border-4 rounded-2xl w-96 md:w-72'>
+      <div className="grid md:grid-cols-2 gap-4 w-full md:w-auto">
+        <a href="https://bsky.app/profile/otter.foo" className='btn btn-lg btn-outline btn-accent border-4 rounded-2xl w-full md:w-72'>
           <IconBrandBluesky className='w-8 h-8' />
           Bluesky
         </a>
-        <a href="link1" className='btn btn-lg btn-outline btn-accent btn-block border-4 rounded-2xl  w-96 md:w-72'>
+        <a href="https://furry.engineer/@alex" className='btn btn-lg btn-outline btn-accent border-4 rounded-2xl w-full md:w-72'>
           <IconBrandMastodon className='w-8 h-8' />
           Mastodon
         </a>
-        <a href="link1" className='btn btn-lg btn-outline btn-accent btn-block border-4 rounded-2xl  w-96 md:w-72'>
+        <a href="https://twitter.com/alexottr" className='btn btn-lg btn-outline btn-accent border-4 rounded-2xl w-full md:w-72'>
           <IconBrandTwitter className='w-8 h-8' />
           Twitter
         </a>
-        <a href="link1" className='btn btn-lg btn-outline btn-accent btn-block border-4 rounded-2xl  w-96 md:w-72'>
+        <a href="https://discordapp.com/users/748482870942629889" className='btn btn-lg btn-outline btn-accent border-4 rounded-2xl w-full md:w-72'>
           <IconBrandDiscord className='w-8 h-8' />
           Discord
         </a>
-        <a href="link1" className='btn btn-lg btn-outline btn-accent btn-block border-4 rounded-2xl  w-96 md:w-72'>
+        <a href="https://t.me/alexottr" className='btn btn-lg btn-outline btn-accent border-4 rounded-2xl w-full md:w-72'>
           <IconBrandTelegram className='w-8 h-8' />
           Telegram
         </a>
       </div>
       <h2 className="text-2xl font-bold my-4 text-white">Projects and Art</h2>
-      <div className="grid md:grid-cols-2 gap-4">
-        <a href="link1" className='btn btn-lg btn-outline btn-accent btn-block border-4 rounded-2xl  w-96 md:w-72'>
+      <div className="grid md:grid-cols-2 gap-4 w-full md:w-auto">
+        <a href="https://github.com/aottr" className='btn btn-lg btn-outline btn-accent border-4 rounded-2xl w-full md:w-72'>
           <IconBrandGithub className='w-8 h-8' />
           Github
         </a>
 
-        <a href="link1" className='btn btn-lg btn-outline btn-accent btn-block border-4 rounded-2xl  w-96 md:w-72'>
+        <a href="https://www.furaffinity.net/user/coy~" className='btn btn-lg btn-outline btn-accent border-4 rounded-2xl w-full md:w-72'>
           <FurAffinity />
           FurAffinity
         </a>
       </div>
       <h2 className="text-2xl font-bold my-4 text-white">Support</h2>
-      <div className="grid md:grid-cols-2 gap-4">
+      <div className="grid md:grid-cols-2 gap-4 w-full md:w-auto">
 
-        <a href="link1" className='btn btn-lg btn-outline btn-accent btn-block border-4 rounded-2xl w-96 md:w-72'>
+        <a href="https://ko-fi.com/alexottr" className='btn btn-lg btn-outline btn-accent border-4 rounded-2xl w-full md:w-72'>
           <IconCoffee className='w-8 h-8' />
           Ko-Fi
         </a>
-        <a href="link1" className='btn btn-lg btn-outline btn-accent btn-block border-4 rounded-2xl w-96 md:w-72'>
+        <a href="https://patreon.com/alexottr" className='btn btn-lg btn-outline btn-accent border-4 rounded-2xl w-full md:w-72'>
           <IconBrandPatreon className='w-8 h-8' />
           Patreon
         </a>
